2c6685a2493db13ce2a311647797eb0748a35256
anaobi <anaobii@gmail.com>
2023-10-24 17:17:00 +0100

    first build

--- recipe/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mkdir $PREFIX/aff3ct/build
-cd $PREFIX/aff3ct/build
-cmake .. -DCMAKE_BUILD_TYPE="Release" -DCMAKE_INSTALL_PREFIX=$PREFIX -DAFF3CT_OVERIDE_VERSION=$VERSION
-make -j4
-make install
