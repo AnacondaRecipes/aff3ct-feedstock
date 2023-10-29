0df69cd224ff7a5a9bbb7b715d8228354e7c999f
anaobi <anaobii@gmail.com>
2023-10-29 11:14:21 +0000

    first build

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,8 +1,28 @@
 #!/bin/bash
 
+
+#git clone --recursive https://github.com/aff3ct/aff3ct.git
+#mkdir -p $PREFIX/aff3ct/build
+#cd $PREFIX/aff3ct/build
+#cmake .. -DCMAKE_BUILD_TYPE="Release" -DCMAKE_INSTALL_PREFIX=$PREFIX -DAFF3CT_OVERIDE_VERSION=$VERSION
+#make -j4
+#make DESTDIR=$PREFIX/aff3ct/build install
+
+#mkdir aff3ct/build
+#cd aff3ct/build
+#cmake .. -DCMAKE_BUILD_TYPE="Release" 
+#cd build
+#make -j4
+#make DESTDIR=$PREFIX/aff3ct install
+
+
+
+cd $PREFIX
+git clone --recursive https://github.com/aff3ct/aff3ct.git
 mkdir $PREFIX/aff3ct/build
 cd $PREFIX/aff3ct/build
-#cmake .. -DCMAKE_BUILD_TYPE="Release" -DCMAKE_INSTALL_PREFIX=$PREFIX -DAFF3CT_OVERIDE_VERSION=$VERSION
-cmake .. -DCMAKE_BUILD_TYPE="Release" 
+cmake .. -DCMAKE_BUILD_TYPE="Release" -DCMAKE_INSTALL_PREFIX=$PREFIX -DAFF3CT_OVERIDE_VERSION=$VERSION
 make -j4
-make install
+make DESTDIR=$PREFIX/aff3ct/build install
+
+
