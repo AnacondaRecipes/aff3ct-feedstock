9feb72119a28e42466be201486a834396ca97578
Bradley Kreider <1699478+bkreider@users.noreply.github.com>
2023-10-31 14:42:42 -0400

    fix build

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,19 +10,17 @@
 
 #mkdir aff3ct/build
 #cd aff3ct/build
-#cmake .. -DCMAKE_BUILD_TYPE="Release" 
+#cmake .. -DCMAKE_BUILD_TYPE="Release"
 #cd build
 #make -j4
 #make DESTDIR=$PREFIX/aff3ct install
 
 
 
-cd $PREFIX
-git clone --recursive https://github.com/aff3ct/aff3ct.git
-mkdir $PREFIX/aff3ct/build
-cd $PREFIX/aff3ct/build
+mkdir aff3ct/build
+cd aff3ct/build
 cmake .. -DCMAKE_BUILD_TYPE="Release" -DCMAKE_INSTALL_PREFIX=$PREFIX -DAFF3CT_OVERIDE_VERSION=$VERSION
 make -j4
-make DESTDIR=$PREFIX/aff3ct/build install
+make install
 
 
