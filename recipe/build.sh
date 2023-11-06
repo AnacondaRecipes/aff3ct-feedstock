8a977190f58dcd4504e71b0369ece7ee14534345
Bradley Kreider <1699478+bkreider@users.noreply.github.com>
2023-11-06 15:39:10 -0500

    Checkout git from inside build.sh for proper git settings

--- recipe/build.sh
+++ recipe/build.sh
@@ -16,7 +16,11 @@
 #make DESTDIR=$PREFIX/aff3ct install
 
 
+git config --global protocol.file.allow always
 
+git clone --recursive https://github.com/aff3ct/aff3ct.git
+cd affe3ct
+git checkout tags/v$VERSION
 mkdir -p build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE="Release" -DCMAKE_INSTALL_PREFIX=$PREFIX -DAFF3CT_OVERIDE_VERSION=$VERSION
