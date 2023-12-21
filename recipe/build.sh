ff6eaf31bd799b04c431e86039dafebe49f8f670
Bradley Kreider <1699478+bkreider@users.noreply.github.com>
2023-12-21 12:13:11 -0500

    Attempt to add py_aff3ct build

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,30 +1,40 @@
 #!/bin/bash
 
 
-#git clone --recursive https://github.com/aff3ct/aff3ct.git
-#mkdir -p $PREFIX/aff3ct/build
-#cd $PREFIX/aff3ct/build
-#cmake .. -DCMAKE_BUILD_TYPE="Release" -DCMAKE_INSTALL_PREFIX=$PREFIX -DAFF3CT_OVERIDE_VERSION=$VERSION
-#make -j4
-#make DESTDIR=$PREFIX/aff3ct/build install
-
-#mkdir aff3ct/build
-#cd aff3ct/build
-#cmake .. -DCMAKE_BUILD_TYPE="Release"
-#cd build
-#make -j4
-#make DESTDIR=$PREFIX/aff3ct install
-
-
 git config --global protocol.file.allow always
 
-git clone --recursive https://github.com/aff3ct/aff3ct.git
+
+# Py_aff3ct centric instructions
+git clone --recursive https://github.com/aff3ct/py_aff3ct.git
+git submodule update --init --recursive
+
+cd lib
+
+
+# If just compiling aff3ct
+#git clone --recursive https://github.com/aff3ct/aff3ct.git
+
 cd aff3ct
 git checkout tags/v$PKG_VERSION
 mkdir -p build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE="Release" -DCMAKE_INSTALL_PREFIX=$PREFIX -DAFF3CT_OVERRIDE_VERSION=$PKG_VERSION
+cmake .. -DCMAKE_BUILD_TYPE="Release" -DCMAKE_INSTALL_PREFIX=$PREFIX -DAFF3CT_OVERRIDE_VERSION=$PKG_VERSION -DAFF3CT_COMPILE_STATIC_LIB="ON" -DAFF3CT_COMPILE_SHARED_LIB="ON"
 make -j4
 make install
 
+
+## From https://github.com/aff3ct/py_aff3ct/blob/master/README.md
+
+
+# Back to py_affect
+cd ../../../..
+
+# copy the CMake configuration files from the AFF3CT build
+mkdir cmake && mkdir cmake/Modules
+cp lib/aff3ct/build/lib/cmake/aff3ct-*/* cmake/Modules
+
+../configure.py --verbose
+cmake .. -G"Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="-Wall -funroll-loops -march=native -fvisibility=hidden -fvisibility-inlines-hidden -faligned-new"
+make -j4
+make install
 
